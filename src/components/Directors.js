7e93d506f439a89f868d4e9166071d2255efff24
Lindsay Mecher <lindsaymecher@gmail.com>
2020-05-21 16:10:54 -0500

    passed all tests.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const renderDirectors = () => {
+    return directors.map(director => {
+      return(
+        <div>
+          <h2>{director.name}</h2>
+          <h2>Movies</h2>
+          <ul>
+            {director.movies.map(movie => {
+              return(
+                <li>
+                  {movie}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,37 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderMovies = () => {
+   return movies.map(movie => {
+     return(
+       <div>
+         <h2>
+           Title:
+           {movie.title}
+         </h2>
+         <h2>
+           Time:
+           {movie.time}
+         </h2>
+         <h2>Genres:</h2>
+         <ul>
+           {movie.genres.map(genre => {
+             return(
+             <li>{genre}</li>
+             )
+           })}
+        </ul>
+       </div>
+     )
+   })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>
+          Movies Page
+        </h1>
+        {renderMovies()}
     </div>
   );
 };
